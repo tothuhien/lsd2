24842d8832cee17e7004e0234b19e3880c6b04a1
Minh Bui <m.bui@anu.edu.au>
2020-04-05 12:56:25 +1000

    add support for CMake and directive USE_LSD2 for using lsd as a library

---
+++ src/lsd.h
@@ -0,0 +1,16 @@
+/*
+ Interface for LSD as a library
+ */
+
+#ifndef LSD_H_
+#define LSD_H_
+
+#ifdef USE_LSD2
+
+/**
+ main function to call LSD
+ */
+int lsd_main( int argc, char** argv );
+#endif
+
+#endif
